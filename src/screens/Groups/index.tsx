8b2b3992fadb8e6d9e20f8c60baee917ae4953bb
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-07 21:26:16 +0000

    added assets and babel-module-resolver to make the imports easier

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,20 @@
+
+import {  Text, View , StyleSheet} from 'react-native';
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent: 'center'
+    }
+})
+
