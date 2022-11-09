752e3b625417dde8a42e5c5357f20df5f79afe60
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-09 10:26:19 +0000

    created the component Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/HightLight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,6 +17,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
+
+        <Input />
 
         <Button 
           title="Criar"
