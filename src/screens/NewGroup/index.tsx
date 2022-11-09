49717a05b9320849aba482adefe05b158374a9c7
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-09 10:38:12 +0000

    styled the placeholder using the hook useTheme

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -18,7 +18,8 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input />
+        <Input
+        placeholder="Nome da turma" />
 
         <Button 
           title="Criar"
