4a83e7dd64368637048654eaed168e63b0edd8ce
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-10 09:14:14 +0000

    created the component ButtoIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,9 @@
+import { ButtonIcon } from "@components/ButtinIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/HightLight";
+import { Input } from "@components/Input";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 export function Players() {
   return (
@@ -12,6 +14,17 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add" 
+        />
+      </Form>
     </Container>
   )
 }
