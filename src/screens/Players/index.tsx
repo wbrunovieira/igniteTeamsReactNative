c2949d81ded2132003e063c7438aadc1aff57118
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-10 09:38:52 +0000

    added FlatList in Player screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,10 +3,15 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/HightLight";
 import { Input } from "@components/Input";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
   return (
     <Container>
       <Header showBackButton />
@@ -29,10 +34,25 @@ export function Players() {
         
       </Form>
 
-      <Filter 
-        title="Time A"
-        isActive
-      />
+
+<HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
