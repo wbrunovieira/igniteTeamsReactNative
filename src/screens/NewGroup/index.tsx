f292209db2f5355e79a6be4e529bcc3a87de3a71
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-11 08:12:51 +0000

    added the SafeAreaView to all screens

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,6 @@
 import { Container, Content, Icon } from "./styles";
 
+
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/HightLight";
