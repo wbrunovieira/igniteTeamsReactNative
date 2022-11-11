502ee6b6c5db63ca0c9d592566753fad9e717d12
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-11 08:06:26 +0000

    created the functions to handle the navigations in the buttons

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,8 +4,15 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/HightLight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+
+  const navigation =useNavigation()
+
+  function handleNew(){
+    navigation.navigate('players', { group: 'Galera'})
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -23,6 +30,7 @@ export function NewGroup() {
 
         <Button 
           title="Criar"
+          onPress={handleNew}
         />
       </Content>
     </Container>
